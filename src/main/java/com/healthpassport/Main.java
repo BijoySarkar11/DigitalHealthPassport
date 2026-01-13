d869e3a79d7c41d8f067342194b7259d894463cb
SABER <saberhossen2004@gmail.com>
2026-01-13 23:43:42 +0600

    update

--- src/main/java/com/healthpassport/Main.java
+++ src/main/java/com/healthpassport/Main.java
@@ -1,9 +1,10 @@
 package com.healthpassport;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Java version = " + System.getProperty("java.version"));
+        System.out.println("HELLO JAVA!");
+
+
     }
 }
---
+++ src/main/java/com/healthpassport/patient.java
@@ -0,0 +1,5 @@
+package com.healthpassport;
+
+public class patient {
+
+}
